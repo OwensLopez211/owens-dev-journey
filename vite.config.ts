50df7c3fb6da4b802813ee5db79fa3ed03cfad11
Owens <owenslopez211@gmail.com>
2025-02-14 13:35:03 -0300

    actualizacion para gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/owens-dev-journey/", // Configura correctamente el subdirectorio en GitHub Pages
+  base: "/", // Configura correctamente el subdirectorio en GitHub Pages
   publicDir: "public", // Asegura que Vite sirva archivos estáticos
 
   build: {
